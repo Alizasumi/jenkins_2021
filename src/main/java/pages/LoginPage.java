be1941cac072eb07a40991f7c0df9481f1d6406e
Alizasumi <aliza_sumi@hotmail.com>
2022-02-13 17:27:04 -0500

    first commit

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package pages;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginPage {
+
+	WebDriver driver;
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	// Element Library
+	@FindBy(how = How.XPATH, using = "//input[@id='username']")
+	WebElement UserName;
+	@FindBy(how = How.XPATH, using = "//input[@id='password']")
+	WebElement Password;
+	@FindBy(how = How.XPATH, using = "//button[@name='login']")
+	 WebElement SignInButton;
+
+
+//Methods to interact with the elements
+	public void enterUserName(String userName) {
+		UserName.sendKeys(userName);
+	}
+
+	public void enterPassword(String password) {
+		Password.sendKeys(password);
+	}
+	public void enterCredential(String userName, String password) {
+		UserName.sendKeys(userName);
+		Password.sendKeys(password);
+	}
+	public void clickSignInButton() {
+		SignInButton.click();
+	}
+
+	public String getPageTitle() {
+		return driver.getTitle();
+	}
+	
+
+}
